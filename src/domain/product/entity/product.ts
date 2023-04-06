bb994f1008f5a355be1b5c40a1c3088ca06d0b0d
Vinicius Boscoa <virb30@gmail.com>
2023-04-05 22:45:53 -0300

    implements notification pattern to product entity

---
+++ src/domain/product/entity/product.ts
@@ -0,0 +1,61 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
+import ProductInterface from "./product.interface";
+
+export default class Product extends Entity implements ProductInterface {
+    protected _id: string;
+    private _name: string;
+    private _price: number;
+
+    constructor(id: string, name: string, price: number) {
+        super();
+        this._id = id;
+        this._name = name;
+        this._price = price;
+        this.validate();
+
+        if (this.notification.hasErrors()) {
+            throw new NotificationError(this.notification.getErrors());
+        }
+    }
+
+    validate() {
+        if (this._id.length === 0) {
+            this.notification.addError({ context: "product", message: "Id is required" });
+        }
+        if (this._name.length === 0) {
+            this.notification.addError({ context: "product", message: "Name is required" });
+        }
+        if (this._price < 0) {
+            this.notification.addError({ context: "product", message: "Price must be greater than 0" });
+        }
+    }
+
+    changeName(name: string): void {
+        this._name = name;
+        this.validate();
+        if (this.notification.hasErrors()) {
+            throw new NotificationError(this.notification.getErrors());
+        }
+    }
+
+    get name(): string {
+        return this._name
+    }
+
+    changePrice(price: number): void {
+        this._price = price;
+        this.validate();
+        if (this.notification.hasErrors()) {
+            throw new NotificationError(this.notification.getErrors());
+        }
+    }
+
+    get price(): number {
+        return this._price
+    }
+
+    get id(): string {
+        return this._id;
+    }
+}
